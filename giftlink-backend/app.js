72c987622175fe897569a99f3f30ac7a0a7576a0
diegokbalz8 <diegocabalceta8@gmail.com>
2024-10-12 16:13:01 -0400

    Back end for regis

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
@@ -20,15 +19,14 @@ connectToDatabase().then(() => {
 
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes'); 
-
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
-
-
+const authRoutes = require("./routes/authRoutes");
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
@@ -37,7 +35,7 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
-
+app.use('/api/auth', authRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
 
